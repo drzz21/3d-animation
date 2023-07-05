9175f803650dc84c965b0425f8d8608d26d287d6
Francisco D <drz.y2k@gmail.com>
2023-07-05 15:02:56 -0600

    Building sound section
    En index.css comentamos el atributo opacity:0, está agregado porque la opacidad se modificará usando gsap

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Iphone from '../assets/images/iphone-14.jpg';
-import HoldingIphone from "../assets/images/iphone-hand.png";
+import HoldingIphone from '../assets/images/iphone-hand.png';
 
 function Jumbotron() {
+	const handleLearnMore = () => {
+		// obtenemos el elemento donde está la seccion de sonido
+		const element = document.querySelector('.sound-section');
+		// usamos un scroll para redirigirnos al elemento, usamos el signo de interrogación para que solo
+		//funcione si el elemento no es nulo, el elemento scrollTo debe recibir las coordenadas top y left,
+		//left es 0 y top es igual a las coordenadas del elemento que obtuvimos con el query, y el comportamiento es smooth
+		//para hacer una transición lenta
+		window.scrollTo({ top: element?.getBoundingClientRect().top,left:0,behavior:'smooth' });
+	};
+
 	return (
 		<div className="jumbotron-section wrapper">
 			<h2 className="title">New</h2>
@@ -16,13 +26,12 @@ function Jumbotron() {
 					<button className="button">Buy</button>
 				</li>
 				<li>
-					<a className="link">Learn more</a>
+					<a className="link" onClick={handleLearnMore}>Learn more</a>
 				</li>
 			</ul>
-			<img className='iphone-img' src={HoldingIphone} alt="iPhone"/>
+			<img className="iphone-img" src={HoldingIphone} alt="iPhone" />
 		</div>
 	);
-	
 }
 
 export default Jumbotron;
